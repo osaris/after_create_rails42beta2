3126de85cd58ba62995bf012150986577a25a09b
Raphaël Emourgeon <raphael.emourgeon@gmail.com>
2014-10-08 14:20:28 +0200

    Add basic test.

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "create a post with a comment should affiliate first_comment_id" do
+
+    p = Post.new(:title => 'test')
+    c = p.comments.build(:content => 'test2')
+    p.save
+
+    assert_equal(c.id, p.first_comment_id)
+  end
+
 end
